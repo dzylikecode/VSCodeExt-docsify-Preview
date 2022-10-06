863be11004c9e05794465d2fc44d03489078c3bd
DZ <1494977853@qq.com>
2022-10-06 20:28:17 +0800

    view the localhost port

--- docs/README.md
+++ docs/README.md
@@ -7,3 +7,5 @@
 - [Synchronized Scrolling](https://marketplace.visualstudio.com/items?itemName=masakit.synchronized-scrolling)
 - [How to Build a VS Code extension for Markdown preview using Remark processor](https://dev.to/salesforceeng/how-to-build-a-vs-code-extension-for-markdown-preview-using-remark-processor-1169)
 - [remark-preview](https://github.com/SubratThakur/remark-preview)
+- [Webview API](https://code.visualstudio.com/api/extension-guides/webview)
+- [vscode-vs-browser](https://github.com/Phu1237/vscode-vs-browser)
--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 const vscode = require("vscode");
 const path = require("path");
+const fs = require("fs");
 async function main(context) {
   vscode.window.showInformationMessage("Hello World from docsify-Preview!");
+  showInVSCode();
+}
+
+const indexFile = path.join(__dirname, "./index.html");
+
+async function getHtmlContent(url) {
+  let text = "";
+  try {
+    text = fs.readFileSync(indexFile, "utf8");
+  } catch (err) {
+    console.log(err);
+  }
+  return formatHtml(text, { url: url });
+  function formatHtml(html, variables) {
+    let newFile = html.replace(
+      /\${(\w+)}/g,
+      (match, varName) => variables[varName] || ""
+    );
+    return newFile;
+  }
+}
+
+async function showInVSCode() {
   const panel = vscode.window.createWebviewPanel(
     // Webview id
     "docify_Previewer",
@@ -15,7 +39,7 @@ async function main(context) {
       retainContextWhenHidden: true,
     }
   );
-  panel.webview.html = await vscode.window.activeTextEditor.document.getText();
+  panel.webview.html = await getHtmlContent("http://localhost:8080/");
 }
 
 module.exports = {
