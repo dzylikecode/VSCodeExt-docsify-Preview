5f96363540a1c2fc27afc15e9d1da21fbaa24479
DZ <1494977853@qq.com>
2022-10-18 13:23:26 +0800

    test

--- src/extension.js
+++ src/extension.js
@@ -11,6 +11,12 @@ function activate(context) {
   );
 
   context.subscriptions.push(disposable);
+  let disposable1 = vscode.commands.registerCommand(
+    "docsify-preview.openInBrowser",
+    async () => main(context, disposable)
+  );
+
+  context.subscriptions.push(disposable1);
 }
 
 // this method is called when your extension is deactivated
